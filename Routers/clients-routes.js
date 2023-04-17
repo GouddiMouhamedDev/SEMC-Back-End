597de65878554a06204494b8b31ad898cab25672
GouddiMouhamedDev <toonmouhamed@gmail.com>
2023-04-17 21:48:28 +0100

    up Clients

---
+++ Routers/clients-routes.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const route = express.Router()
+const db = require('../models')
+
+route.post('/createclient', (req, res, next) => {
+  db.Product.create(req.body)
+    .then((response) => res.status(200).send(response))
+    .catch((err) => res.status(400).send(err))
+})
+
+
+
+route.get('/clients/:id', (req, res, next) => {
+  db.Product.findOne({ where: { id: req.params.id } })
+    .then((response) => res.status(201).send(response))
+    .catch((err) => res.status(400).send(err))
+})
+
+
+route.get('/clients', (req, res, next) => {
+  db.Product.findAll()
+    .then((response) => res.status(200).send(response))
+    .catch((err) => res.status(400).send(err))
+})
+
+route.patch('/client/:id', (req, res, next) => {
+  db.Product.update(req.body, { where: { id: req.params.id } })
+    .then((response) => {
+      
+      db.Product.findOne({ where: { id: req.params.id } })
+      .then((response) => res.status(201).send(response))
+      .catch((err) => res.status(400).send(err))
+      
+    })
+    .catch((err) => res.status(400).send(err))
+})
+
+route.delete('/client/:id', (req, res, next) => {
+  db.Product.destroy({ where: { id: req.params.id } })
+    .then((response) => { res.status(204).send() })
+    .catch((err) => res.status(400).send(err))
+})
+module.exports = route
---
+++ models/Clients.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+    const Clients = sequelize.define("Clients", {
+      Nom: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      Solde: {
+        type: DataTypes.DECIMAL(10, 3),
+        allowNull: false
+      },
+      
+      Mat:{
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      Num:{
+        type: DataTypes.STRING,
+        allowNull:true
+      }
+    });
+  
+    return Clients;
+  };
+  
