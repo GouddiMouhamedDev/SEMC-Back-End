efa5e1fd4a95b593a2cfe35c5d922204b9400313
GouddiMouhamedDev <toonmouhamed@gmail.com>
2023-05-01 10:32:39 +0100

    Update server.js
    + de opti

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const port=3000
-const express=require("express")
-const app= express()
-const db=require('./models')
-const userRoutes=require("./Routers/user-routes")
-const productRoutes=require("./Routers/product-routes")
-const registerRoutes=require("./Routers/register-routes")
-const clientsRoutes=require("./Routers/clients-routes")
-const DevisRoutes=require("./Routers/Devis-routes")
-const contDevisRoutes=require("./Routers/contDevis-routes")
-const cors = require('cors');
-app.use(cors());
- app.use(express.urlencoded({extended:true}))
- app.use(express.json())
-app.use('/',userRoutes)
-app.use('/',productRoutes)
-app.use('/',registerRoutes)
-app.use('/',clientsRoutes)
-app.use('/',DevisRoutes)
-app.use('/',contDevisRoutes)
+const port = 3000;
+const express = require("express");
+const app = express();
+const db = require("./models");
+const cors = require("cors");
 
- db.sequelize.sync().then(()=>{
-   app.listen(port, () => {
-      console.log(`Server started on port ${port}`)
-    })
- }) 
+const routes = [
+  require("./Routers/user-routes"),
+  require("./Routers/product-routes"),
+  require("./Routers/register-routes"),
+  require("./Routers/clients-routes"),
+  require("./Routers/Devis-routes"),
+  require("./Routers/contDevis-routes")
+];
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+routes.forEach((route) => {
+  app.use("/", route);
+});
+
+db.sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+});
